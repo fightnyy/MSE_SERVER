867fb7118d932e096232ef9b045c68c8850beba9
fightnyy <fightnyy@naver.com>
2021-05-06 14:07:14 +0900

    first_commit

---
+++ src/main/java/mse/exam/SilvernineApplication.java
@@ -0,0 +1,13 @@
+package mse.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilvernineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilvernineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mse/exam/SilvernineApplicationTests.java
@@ -0,0 +1,13 @@
+package mse.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvernineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
